e07acd306ffc4e2e5c6b69655c511a6743636084
tZandr <alex_tjernstrom@hotmail.com>
2026-04-13 14:16:01 +0200

    Conditional rendering

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,13 +1,16 @@
 import type { UserCardProps } from '../App';
 
 export const UserCard = ({ user }: UserCardProps) => {
-
   return (
-    <div className='userCard'>
+    <div className="userCard">
       <div>
         <h2>{user.name}</h2>
         <p>{user.email}</p>
-        <p>{user.city}</p>
+        <p>{user.address.city}</p>
+        {user.address.city === 'Wisokyburgh' && (
+          <small>Ingen av dom här städerna existerar</small>
+          // Läste fel; trodde vi skulle använda API så det får bli såhär
+        )}
       </div>
 
       <button
